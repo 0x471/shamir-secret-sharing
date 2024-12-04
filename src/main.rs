741c350871a4b5d2e5cfc377fd8657125a1b71db
0x471 <0x471@protonmail.com>
2024-12-04 13:56:26 +0000

    implement polynomial generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use ark_bn254::Fr as Field;
+use ark_ff::{Field as ArkField, UniformRand};
+use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial, Polynomial};
+use rand::rngs::OsRng;
+
+struct Point {
+    x: Field,
+    y: Field,
+}
+
+struct ShamirSecretShare {
+    threshold: usize,
+    total_shares: usize,
+}
+
+impl ShamirSecretShare {
+    fn new(threshold: usize, total_shares: usize) -> Self {
+        Self {
+            threshold,
+            total_shares,
+        }
+    }
+
+    fn generate_polynomial(
+        secret: Field,
+        degree: usize,
+        rand: &mut OsRng,
+    ) -> DensePolynomial<Field> {
+        let mut coefficients = vec![secret];
+        for _ in 1..degree {
+            let coefficient = Field::rand(rand);
+            coefficients.push(coefficient)
+        }
+        DensePolynomial::from_coefficients_vec(coefficients)
+    }
 }
