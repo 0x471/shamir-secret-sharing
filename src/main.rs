3efca7bebd40efe776fb9924ce2e8b6bdf8cf72e
0x471 <0x471@protonmail.com>
2024-12-04 14:11:56 +0000

    implement lagrange interpolation for secret reconstruction

--- src/main.rs
+++ src/main.rs
@@ -37,11 +37,38 @@ impl ShamirSecretShare {
     fn generate_shares(&self, secret: Field, degree: usize, rand: &mut OsRng) -> Vec<Point> {
         let poly = Self::generate_polynomial(secret, degree, rand);
         (1..=self.total_shares)
-        .map(|i| {
-            let x = Field::from(i as u64);
-            let y = poly.evaluate(&x);
-            Point { x, y }
-        })
-        .collect()
+            .map(|i| {
+                let x = Field::from(i as u64);
+                let y = poly.evaluate(&x);
+                Point { x, y }
+            })
+            .collect()
+    }
+
+    fn reconstruct_secret(shares: &[Point], threshold: usize) -> Field {
+        let mut secret = Field::from(0);
+
+        for i in 0..threshold {
+            let point_x = &shares[i];
+            let x_i = point_x.x;
+            let y_i = point_x.y;
+            let mut lagrange_coeff = Field::from(1);
+
+            for j in 0..threshold {
+                if i != j {
+                    let point_j = &shares[j];
+                    let x_j = point_j.x;
+                    let y_j = point_j.y;
+                    let numerator = Field::from(0) - x_j; // -x_j
+                    let denominator = x_i - x_j; // x_i - x_j
+                    lagrange_coeff *= numerator * denominator.inverse().unwrap();
+                    // Modular inverse
+                }
+            }
+
+            secret += lagrange_coeff * y_i;
+        }
+
+        secret
     }
 }
